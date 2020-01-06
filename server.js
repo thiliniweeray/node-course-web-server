0ef4d468e7dec8360de349e6d26ce8a5f13d08d7
Thilini Weerasuriya <90945551@westernsydney.edu.au>
2020-01-07 10:49:36 +1100

    Added projects page

--- server.js
+++ server.js
@@ -43,8 +43,14 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'About Page',
+        pageTitle: 'About Page'
     });
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+    })
 });
 
 app.get('/bad', (req, res) => {
